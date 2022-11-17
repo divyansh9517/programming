6358418093db8b325865e8d91ca8234c164470a0
Divyansh Saxena <117467988+divyansh9517@users.noreply.github.com>
2022-11-17 22:03:11 +0530

    Add files via upload

---
+++ chANGECASE.java
@@ -0,0 +1,11 @@
+class chANGECASE
+{
+    public static void main(String []abc)
+    {
+        String s1= "i love you";
+        System.out.println("the i love you text consists the length :-"+s1.length());
+        System.out.println("the given string is :- "+s1);
+        System.out.println("the upper case :- "+s1.toUpperCase());
+        System.out.println("the lower case is :- "+s1.toLowerCase());
+    }
+}
